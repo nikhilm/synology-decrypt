7f1306b11adb2f7cff0781594c22dffd0527b2de
Nikhil Marathe <me@nikhilism.com>
2022-12-18 13:10:17 +0530

    Dead code removal

--- main.rkt
+++ main.rkt
@@ -14,8 +14,6 @@
 (module+ test
   (require rackunit))
 
-; TODO: Actually do this side-effecting state either in main or see the parameterize stuff
-(crypto-factories (list libcrypto-factory))
 (define my-kdf (get-kdf (list 'pbkdf2 'hmac 'md5)))
 
 (define (repeated-hash start count)
@@ -48,28 +46,6 @@
       [(list key iv)
        (decrypt '(aes cbc) key iv data-bytes #:pad #t)])))
 
-(define (salted-hash salt input)
-  (md5 (bytes-append salt input)))
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
-
 (define (decrypt-ports password input-port output-port)
   (define parsed-parts (parse-synology-port input-port))
   (define encryption-struct (first parsed-parts))
@@ -80,12 +56,9 @@
 
 
 (module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
   (require racket/cmdline)
+
+  (crypto-factories (list libcrypto-factory))
 
   ; TODO: Allow password file to be specified on the command line
   (define (decrypt-file input output)
