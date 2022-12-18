2a343f1e8fb11cf7f1c58ced1decb8cda2b81763
Nikhil Marathe <me@nikhilism.com>
2022-12-18 12:58:15 +0530

    Generate deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,10 @@
 #lang info
 (define collection "synology-decrypt")
-(define deps '("base"))
+(define deps '("base64-lib"
+               "binfmt"
+               "crypto-lib"
+               "lz4-lib"
+               "base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/synology-decrypt.scrbl" ())))
 (define pkg-desc "Description Here")
