168b369d162d89abc24acc09e94657ee755a785a
Nikhil Marathe <me@nikhilism.com>
2022-12-18 12:50:19 +0530

    working implementation based on copying and tweaking code from synology-decrypt-trial

--- main.rkt
+++ main.rkt
@@ -1,8 +1,55 @@
 #lang racket/base
+(require file/md5)
+(require racket/port)
+(require racket/list)
+(require racket/match)
+(require base64)
+(require crypto)
+(require crypto/libcrypto)
+(require (only-in file/sha1 hex-string->bytes))
+(require file/lz4)
+
+(require "parser.rkt")
 
 (module+ test
   (require rackunit))
 
+; TODO: Actually do this side-effecting state either in main or see the parameterize stuff
+(crypto-factories (list libcrypto-factory))
+(define my-kdf (get-kdf (list 'pbkdf2 'hmac 'md5)))
+
+(define (repeated-hash start count)
+  (for/fold ([output start])
+            ([i (in-range count)])
+    (md5 output #f)))
+
+; this is convoluted, can we do better?
+(define (openssl-kdf-iter pwd salt key-size iv-size key-iv-buffer temp)
+  (if (< (bytes-length key-iv-buffer) (+ key-size iv-size))
+      (let ([temp (repeated-hash (bytes-append temp pwd salt) (if (eq? 0 (bytes-length salt)) 1 1000))])
+        (openssl-kdf-iter pwd salt key-size iv-size (bytes-append key-iv-buffer temp) temp))
+      key-iv-buffer))
+
+(define (openssl-kdf pwd salt key-size iv-size)
+  (let* ([count 1000]
+         [key-iv-output (openssl-kdf-iter pwd salt key-size iv-size (bytes) (bytes))])
+    (list (subbytes key-iv-output 0 key-size) (subbytes key-iv-output key-size))
+    ))
+
+
+(define (decrypted-enc1-key password encryption-struct)
+  (match (openssl-kdf password (encryption-information-salt encryption-struct) 32 16)
+    [(list key iv) (decrypt '(aes cbc)
+                            key iv (encryption-information-enc_key1 encryption-struct) #:pad #t)]))
+
+(define (decrypt-data password encryption-struct data-bytes)
+  (let ([enc1-key (decrypted-enc1-key password encryption-struct)])
+    (match (openssl-kdf (hex-string->bytes (bytes->string/latin-1 enc1-key)) (bytes) 32 16)
+      [(list key iv)
+       (decrypt '(aes cbc) key iv data-bytes #:pad #t)])))
+
+(define (salted-hash salt input)
+  (md5 (bytes-append salt input)))
 ;; Notice
 ;; To install (from within the package directory):
 ;;   $ raco pkg install
@@ -24,16 +71,13 @@
 ;; http://docs.racket-lang.org/style/index.html
 
 (define (decrypt-ports password input-port output-port)
-  (printf "TODO to perform decryption~n"))
+  (define parsed-parts (parse-synology-port input-port))
+  (define encryption-struct (first parsed-parts))
+  (define file-bytes (second parsed-parts))
+  (define metadata (third parsed-parts))
+  (define plaintext (decrypt-data password encryption-struct file-bytes))
+  (lz4-decompress-through-ports (open-input-bytes plaintext) output-port))
 
-
-
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
-
-  (check-equal? (+ 2 2) 4))
 
 (module+ main
   ;; (Optional) main submodule. Put code here if you need it to be executed when
@@ -45,7 +89,7 @@
 
   ; TODO: Allow password file to be specified on the command line
   (define (decrypt-file input output)
-    (define password (read-line))
+    (define password (read-bytes-line))
     (call-with-input-file* input
       (lambda (input-port)
         (call-with-output-file* output
---
+++ parser.rkt
@@ -0,0 +1,127 @@
+#lang racket/base
+
+(require racket/contract)
+(require racket/match)
+(require racket/dict)
+(require base64)
+(require (prefix-in synology: "format.b"))
+
+(module+ test
+  (require rackunit))
+
+(struct encryption-information
+  (compressed
+   digest-type
+   enc_key1
+   enc_key2
+   encrypted
+   file_name
+   key1_hash
+   key2_hash
+   salt
+   session_key_hash
+   version)
+  #:transparent)
+
+; Thanks to binfmt author Bogdan Popa for this helper function.
+(define ref
+  (case-lambda
+    [(id v)
+     (define p (assq id v))
+     (unless p
+       (error 'ref "key not found: ~s~n  have: ~e" id (map car v)))
+     (cdr p)]
+    [(id . args)
+     (ref id (apply ref args))]))
+
+(define (u8-list? l)
+  (equal? (car l) 'u8_1))
+
+(define/contract (u8-list-to-string u8-list)
+  (u8-list? . -> . string?)
+  (bytes->string/utf-8 (u8-list-to-bytes u8-list)))
+
+(define/contract (u8-list-to-bytes u8-list)
+  (u8-list? . -> . bytes?)
+  (apply bytes (cdr u8-list)))
+
+(define (kv-pair? l)
+  (and (equal? (length l) 2)
+       (equal? (car (car l)) 'key_1)
+       (equal? (car (cadr l)) 'value_1)))
+
+(define (parse-primitive prim)
+  (case (car prim)
+    [(string-rest_1) (u8-list-to-string (list-ref prim 2))]
+    [(bytes-rest_1) (u8-list-to-bytes (list-ref prim 2))]
+    [(int-rest_1) (cadr (list-ref prim 2))]
+    [(dict-entry_1) (parse-dict-entry prim)]
+    [else (error "no primitive match ~a" (car prim))]))
+
+(define/contract (parse-kv kv)
+  (kv-pair? . -> . any)
+  ; index 1 is always '(num_1 . <n>) and index 2 is the primitive
+  `(,(parse-primitive (list-ref (car kv) 2)) . ,(parse-primitive (list-ref (cadr kv) 2))))
+
+(module+ test
+  (check-equal? (parse-kv '((key_1 (num_1 . 16) (string-rest_1 (length_1 . 4) (u8_1 116 121 112 101)))
+                            (value_1 (num_1 . 16) (string-rest_1 (length_1 . 8) (u8_1 109 101 116 97 100 97 116 97)))))
+                '("type" . "metadata")))
+
+(define (parse-dict-entry de)
+  (case (car de)
+    [(dict-entry_1)
+     (for/list [(kv (in-list (cdr de)))]
+       (parse-kv kv))]
+    [else "parse-dict-entry fail"]))
+
+(define (parse-dict d)
+  ; prefix-byte (dict-entry_1 with actual dictionary entries) end-byte
+  (make-hash (parse-dict-entry (list-ref d 1))))
+
+(define (parse-dicts ds)
+  ; (dict_1 dict dict dict)
+  (for/list [(d (in-list (cdr ds)))]
+    (parse-dict d)))
+
+(define (parse-string string-rest)
+  (caddr string-rest))
+
+(define (parse-whole whole)
+  (car
+   (for/list ([elem (in-list whole)]
+              #:when (equal? (car elem) 'dict_1))
+     (parse-dicts elem))))
+
+; The problem with data being a dictionary is we have to parse the whole file into memory before we can process it.
+; We would much rather assume that the metadata dictionary comes first, and then the data dictionary is streamed somehow, and then the md5 metadata dict can be read later (or can be read from the end)
+
+(define (all-dicts? ds)
+  (for/and ([d (in-list ds)])
+    (dict? d)))
+
+(define (make-encryption-information d)
+  (encryption-information
+   (equal? 1 (dict-ref d "compress"))
+   (dict-ref d "digest")
+   (base64-decode (dict-ref d "enc_key1"))
+   (base64-decode (dict-ref d "enc_key2"))
+   (equal? 1 (dict-ref d "encrypt"))
+   (dict-ref d "file_name")
+   (dict-ref d "key1_hash")
+   (dict-ref d "key2_hash")
+   (string->bytes/latin-1 (dict-ref d "salt"))
+   (dict-ref d "session_key_hash")
+   (dict-ref d "version")))
+
+(define/contract (three-dicts-to-synology-file dicts)
+  (all-dicts? . -> . any)
+  (list (make-encryption-information (car dicts)) (dict-ref (list-ref dicts 1) "data") (list-ref dicts 2)))
+
+(define (parse-synology-port input-port)
+  (let ([dicts (parse-whole (synology:format input-port))])
+    (three-dicts-to-synology-file dicts)))
+
+(provide
+ parse-synology-port
+ (struct-out encryption-information))
